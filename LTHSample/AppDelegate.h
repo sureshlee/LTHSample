437eb3b8d520e9c9b6f816a26690058f6d0a0d5b
tringSuresh <suresh.r@tringapps.com>
2016-11-11 11:31:18 +0530

    Initial Commit

---
+++ LTHSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LTHSample
+//
+//  Created by Suresh on 11/11/16.
+//  Copyright © 2016 tringapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LTHSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LTHSample
+//
+//  Created by Suresh on 11/11/16.
+//  Copyright © 2016 tringapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
